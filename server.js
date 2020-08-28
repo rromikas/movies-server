086d038826be4e945e5634a9bdc395041ead64a9
rromikas <rromikas@gmail.com>
2020-08-29 01:45:16 +0300

    ok

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ const path = require("path");
 const helmet = require("helmet");
 require("./api/DatabaseApi")();
 
-app.use(helmet());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 app.use(cors());
 app.use(bodyParser.json());
 
